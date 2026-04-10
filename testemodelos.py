40102316f5f3d485fc8dd736ef3f9b6b41a870be
Gabriel <gabrielcorreasouza46@gmail.com>
2026-04-10 10:58:05 -0300

    fix

--- testemodelos.py
+++ testemodelos.py
@@ -3,9 +3,9 @@ import google.generativeai as genai
 
 genai.configure(api_key="SUA API KEY")
 
-print("🔍 Buscando modelos disponíveis para esta chave...\n")
+print(" Buscando modelos disponíveis para esta chave...\n")
 
 
 for m in genai.list_models():
     if 'generateContent' in m.supported_generation_methods:
-        print(f"✅ NOME EXATO: {m.name}")
+        print(f" NOME EXATO: {m.name}")
